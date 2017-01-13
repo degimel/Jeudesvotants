a14c8b54760d5d8d988390ccb3f6815a39dd7b7f
david <danieli.davidalexandre@gmail.com>
2017-01-14 00:03:50 +0100

    Ajout de fonctions intermédiaires

--- Simu.cc
+++ Simu.cc
@@ -1,6 +1,42 @@
 #include "Simu.hh"
 
 
+int Simu::run(SDL_Surface* ecran,SDL_Surface *helico,SDL_Surface *couteau,SDL_Surface *bombe, SDL_Surface *votant1,SDL_Surface *votant2,SDL_Rect& positionHelico, SDL_Event& event, int &continuer){
+	/*On regarde si le joueur a appuyer sur des touches du clavier*/
+	gererEvenementsClavier(event, continuer, positionHelico);
+	
+	/*mise à jour des projectiles*/
+	MajAffichageProjectiles(ecran,helico,couteau,bombe,positionHelico);		
+
+	/*verifier si certains rentrent*/
+	checkEntrants();
+	
+	/*verifier si certains sont arrivés : arrivée <=> abscisse=650*/
+	checkArrivees();
+			
+	/*Mise à jour des electeurs*/
+	MajAffichageElecteurs(ecran,votant1,votant2);			
+	
+	/*Verifier impacts et supprimer des bonhommes si nécessaire*/
+	supprimer_bonhomme1();
+	supprimer_bonhomme2();
+	
+	/*mise à jour de la fenêtre*/
+	SDL_Flip(ecran);
+	
+	/*Mise à jour du temps courant*/
+	_tpsCourant++;
+	
+	return _tpsCourant;
+}
+
+/************************************************************************************************
+						Fonctions intermédiaires appelées dans run()
+*************************************************************************************************/
+
+/*Initialisation de la simulation : cette fonction crée les listes de votants en fonction des 
+paramètres du jeu et initialise aléatoirement (loi uniforme sur la durée de la simulation) leur
+temps de départ*/ 
 void Simu::initialise(){
 		for(int i=0;i<NBTOTALVOTANT1;i++){
 			Electeur electeur;
@@ -15,54 +51,7 @@ void Simu::initialise(){
 		}
 		for(auto it=joueur2.getlistVotants().begin();it<joueur2.getlistVotants().end();it++){
 			it->initialise_tpsDepart(_tpsSimulation);
-		}
-		
-}
-
-/*Bonhommes blancs*/
-void Simu::supprimer_bonhomme1(){
-	for(auto it1=_listProjectiles.begin();it1!=_listProjectiles.end();++it1){
-		if((*it1)->getposition().y>550){
-			Projectile* copieProjectile=(*it1);
-			_listProjectiles.erase(it1);
-			delete (copieProjectile);
-			this->supprimer_bonhomme1();
-			return ;	
-		}
-		for(auto it2=_listElecteur2.begin();it2!=_listElecteur2.end();++it2){
-			if((*it1)->verifieImpact(&(*it2))){
-				Projectile* copieProjectile=(*it1);
-				_listElecteur2.erase(it2);
-				_listProjectiles.erase(it1);
-				delete (copieProjectile); 
-				this->supprimer_bonhomme1();
-				return ;				
-			}					
 		}	
-	}
-}
-
-/*Bonhommes noirs*/
-void Simu::supprimer_bonhomme2(){
-	for(auto it1=_listProjectiles.begin();it1!=_listProjectiles.end();++it1){
-		if((*it1)->getposition().y>550){
-			Projectile* copieProjectile=(*it1);
-			_listProjectiles.erase(it1);
-			delete (copieProjectile);
-			this->supprimer_bonhomme2();
-			return;	
-		}			
-		for(auto it2=_listElecteur1.begin();it2!=_listElecteur1.end();++it2){
-			if((*it1)->verifieImpact(&(*it2))){
-				Projectile* copieProjectile=(*it1);
-				_listElecteur1.erase(it2);
-				_listProjectiles.erase(it1);
-				delete (copieProjectile);
-				this->supprimer_bonhomme2();
-				return;				
-			}					
-		}	
-	}
 }
 
 void Simu::gererEvenementsClavier(SDL_Event& event,int &continuer,SDL_Rect& positionHelico){
@@ -113,17 +102,9 @@ void Simu::MajAffichageProjectiles(SDL_Surface* ecran,SDL_Surface *helico,SDL_Su
 			SDL_BlitSurface(bombe, NULL, ecran, &rect);
 		}
 	}
-	
 }
 
-int Simu::run(SDL_Surface* ecran,SDL_Surface *helico,SDL_Surface *couteau,SDL_Surface *bombe, SDL_Surface *votant1,SDL_Surface *votant2,SDL_Rect& positionHelico, SDL_Event& event, int &continuer){
-	/*On regarde si le joueur a appuyer sur des touches du clavier*/
-	gererEvenementsClavier(event, continuer, positionHelico);
-	
-	//mise à jour des projectiles
-	MajAffichageProjectiles(ecran,helico,couteau,bombe,positionHelico);		
-
-	//verifier si certains rentrent
+void Simu::checkEntrants(){
 	for(auto it=joueur1.getlistVotants().begin();it<joueur1.getlistVotants().end();it++){
 		if(it->get_tpsDepart()==_tpsCourant){
 			_listElecteur1.push_back(*it);
@@ -135,16 +116,16 @@ int Simu::run(SDL_Surface* ecran,SDL_Surface *helico,SDL_Surface *couteau,SDL_Su
 			_listElecteur2.push_back(*it);
 		}
 	}
-	
-	//verifier si certains sont arrivés : arrivée <=> abscisse=650
-	
+}
+
+void Simu::checkArrivees(){
 	for(auto it=_listElecteur1.begin(); it!=_listElecteur1.end();++it){
 		if(it->arrivee(650)){
 			_listElecteur1.erase(it); //retirer l'élement it de la liste
 			cpt_vote_pour_1++;
 			std::cout << "Score actuel : vous " << cpt_vote_pour_1 << "-" << cpt_vote_pour_2 << " adversaire"<< std::endl; 
-			break; //pour eviter le segmentation fault
-			
+			checkArrivees();
+			return;
 		}
 	}
 	
@@ -153,12 +134,13 @@ int Simu::run(SDL_Surface* ecran,SDL_Surface *helico,SDL_Surface *couteau,SDL_Su
 			_listElecteur2.erase(it);
 			cpt_vote_pour_2++;
 			std::cout << "Score actuel : vous " << cpt_vote_pour_1 << "-" << cpt_vote_pour_2 << " adversaire"<< std::endl; 
-			break; //pour éviter le segmentation fault
-			
+			checkArrivees();
+			return;	
 		}
-	}		
-	//Mise à jour des electeurs
-									
+	}
+}
+
+void Simu::MajAffichageElecteurs(SDL_Surface* ecran,SDL_Surface *votant1,SDL_Surface *votant2){
 	for(auto it=_listElecteur1.begin(); it!=_listElecteur1.end();it++){
 		SDL_Rect rect=(*it).update();
 		SDL_BlitSurface(votant1, NULL, ecran, &rect);
@@ -168,12 +150,50 @@ int Simu::run(SDL_Surface* ecran,SDL_Surface *helico,SDL_Surface *couteau,SDL_Su
 		SDL_Rect rect=(*it).update();
 		SDL_BlitSurface(votant2, NULL, ecran, &rect);
 	}
+}
 
-	//Verifier impacts
-	supprimer_bonhomme1();
-	supprimer_bonhomme2();
-	
-	SDL_Flip(ecran);
-	_tpsCourant++;
-	return _tpsCourant;
+/*Bonhommes blancs - soutient joueur*/
+void Simu::supprimer_bonhomme1(){
+	for(auto it1=_listProjectiles.begin();it1!=_listProjectiles.end();++it1){
+		if((*it1)->getposition().y>550){
+			Projectile* copieProjectile=(*it1);
+			_listProjectiles.erase(it1);
+			delete (copieProjectile);
+			this->supprimer_bonhomme1();
+			return;	
+		}
+		for(auto it2=_listElecteur2.begin();it2!=_listElecteur2.end();++it2){
+			if((*it1)->verifieImpact(&(*it2))){
+				Projectile* copieProjectile=(*it1);
+				_listElecteur2.erase(it2);
+				_listProjectiles.erase(it1);
+				delete (copieProjectile); 
+				this->supprimer_bonhomme1();
+				return;				
+			}					
+		}	
+	}
+}
+
+/*Bonhommes noirs - soutient ordinateur*/
+void Simu::supprimer_bonhomme2(){
+	for(auto it1=_listProjectiles.begin();it1!=_listProjectiles.end();++it1){
+		if((*it1)->getposition().y>550){
+			Projectile* copieProjectile=(*it1);
+			_listProjectiles.erase(it1);
+			delete (copieProjectile);
+			this->supprimer_bonhomme2();
+			return;	
+		}			
+		for(auto it2=_listElecteur1.begin();it2!=_listElecteur1.end();++it2){
+			if((*it1)->verifieImpact(&(*it2))){
+				Projectile* copieProjectile=(*it1);
+				_listElecteur1.erase(it2);
+				_listProjectiles.erase(it1);
+				delete (copieProjectile);
+				this->supprimer_bonhomme2();
+				return;				
+			}					
+		}	
+	}
 }
--- Simu.hh
+++ Simu.hh
@@ -5,8 +5,11 @@
 #include "Couteau.hh"
 #include <list>
 #include <vector>
+
+/*Paramètres du jeu*/
 #define NBTOTALVOTANT1 40
 #define NBTOTALVOTANT2 60
+
 class Simu{
 	private :
 		int _tpsCourant;
@@ -19,14 +22,20 @@ class Simu{
 		std::vector<Electeur> _listElecteur1; //votent pour 1
 		std::vector<Electeur> _listElecteur2; //votent pour 2
 	public:
+		/*Méthodes*/
 		Simu(int tpsSimulation): _tpsSimulation(tpsSimulation){cpt_vote_pour_1=0; cpt_vote_pour_2=0;_tpsCourant=0;};
 		~Simu(){};
 		void initialise();
 		int run(SDL_Surface *ecran,SDL_Surface* helico,SDL_Surface *couteau,SDL_Surface *bombe,SDL_Surface *votant1,SDL_Surface *votant2,SDL_Rect& positionHelico,SDL_Event& event, int &continuer);
 		void gererEvenementsClavier(SDL_Event& event,int &continuer,SDL_Rect& positionHelico);
 		void MajAffichageProjectiles(SDL_Surface* ecran,SDL_Surface *helico,SDL_Surface *couteau,SDL_Surface *bombe,SDL_Rect& positionHelico);
+		void checkEntrants();
+		void checkArrivees();
+		void MajAffichageElecteurs(SDL_Surface* ecran,SDL_Surface *votant1,SDL_Surface *votant2);
 		void supprimer_bonhomme1();
 		void supprimer_bonhomme2();
+		
+		/*Getter*/
 		int getcpt1(){ return cpt_vote_pour_1;};
 		int getcpt2(){ return cpt_vote_pour_2;};
 		int get_tpsSimulation(){ return _tpsSimulation;};
